64d4d218a76d4941aeb74ec3800b67d9cc026f1d
yuchen6 <dev.chenyu@gmail.com>
2020-12-19 23:17:02 +0800

    update pages/pay

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,45 @@
 // pages/pay/index.js
+import {
+  chooseAddress,
+  showModal,
+  showToast
+} from "../../utils/asyncWx";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    address: {},
+    cart: [],
+    totalPrice: 0,
+    totalNum: 0
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
+    //获取缓存中的收货地址信息
+    const address = wx.getStorageSync("address");
+    //获取缓存中的购物车数据
+    let cart = wx.getStorageSync("cart") || [];
+    //过滤后的购物车数据
+    cart = cart.filter(v => v.checked);
+    this.setData({
+      address
+    });
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    //总价格 数量
+    let totalPrice = 0;
+    let totalNum = 0;
+    cart.forEach(v => {
+      
+        totalPrice += v.num * v.goods_price;
+        totalNum += v.num;
+      
+    });
+  
+    this.setData({
+      cart,
+      totalPrice,
+      totalNum,
+      address
+    });
+    
   }
 })
