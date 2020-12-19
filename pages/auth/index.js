58c505f19ae69d117c1538253c425cb665648935
yuchen6 <dev.chenyu@gmail.com>
2020-12-20 00:46:21 +0800

    update pages/auth

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,66 +1,34 @@
 // pages/auth/index.js
+import {
+  request
+} from "../../request/index.js";
+import {
+  login
+} from "../../utils/asyncWx";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取用户信息
+  async handleGetUserInfo(e) {
+    try{
+      // 获取用户信息
+    const {
+      encryptedData,
+      rawData,
+      iv,
+      signature
+    } = e.detail;
+    // 获取小程序登陆成功后的code
+    const { code } = await login();
+    const loginParams = { encryptedData,rawData,iv,signature,code};
+    // 发送请求 获取用户的token 需要企业账号否则返回为null
+    const token = await request({url:"/users/wxlogin",data:loginParams, method:"post"});
+    // 把token存入缓存中 同时跳回上一个页面
+    wx.setStorageSync("token", token);
+    wx.navigateBack({
+      delta: 1
+    });
+    } catch (error) {
+      console.log(error);
+    }
   }
 })
