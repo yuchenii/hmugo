7baf97126622c7a13995901409b865e5a497d3e0
yuchen <dev.chenyu@gmail.com>
2021-09-04 19:45:56 +0800

    complete pages/user

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,11 +1,18 @@
 // pages/login/index.js
 Page({
-  handleGetUserInfo(e){
-    //console.log(e);
-    const {userInfo} = e.detail;
-    wx.setStorageSync("userinfo", userInfo);
-    wx.navigateBack({
-      delta: 1
-    });
+ 
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res);
+        wx.setStorageSync("userinfo", res.userInfo);
+        wx.navigateBack({
+          delta: 1
+        });
+      }
+    })
   }
 })
